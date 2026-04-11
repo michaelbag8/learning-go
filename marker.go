40fd2846b02a1c4dd73d26c015e407d9cdc40b10
michaelbag8 <michaelbag8@gmail.com>
2026-04-11 23:43:15 +0100

    fix: marker function

--- marker.go
+++ marker.go
@@ -33,10 +33,53 @@ func makerPrint(word string) int {
 
 	return count
 }
+
+// maler functions that capture (marker, N)
+func maker(word string) string {
+	word = strings.ReplaceAll(word, ", ", ",")
+	words := strings.Fields(word)
+
+	for i := 0; i < len(words); i++ {
+		if words[i] == "(up)" {
+			if i > 0 {
+				words[i-1] = strings.ToUpper(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+			continue
+		}
+		if strings.HasPrefix(words[i], "(") && strings.HasSuffix(words[i], ")") {
+			words[i] = strings.Trim(words[i], "()")
+			if strings.Contains(words[i], ",") {
+				inner := strings.Split(words[i], ",")
+				n, err := strconv.Atoi(inner[1])
+				if err == nil {
+					start := i - n
+					if start < 0 {
+						start = 0
+					}
+					for j := start; j < i; j++ {
+						words[j] = strings.ToUpper(words[j])
+					}
+					words = append(words[:i], words[i+1:]...)
+					i--
+				}
+			}
+		}
+
+	}
+	return strings.Join(words, " ")
+
+}
+
+
 func main() {
 	word := "This is so exciting (up,4)"
 
 	fmt.Println(makerPrint(word))
+
+	fmt.Println(maker("This is so exciting (up)"))
+	fmt.Println(maker("This is so exciting so big so small (up, 3)"))
 
 }
 
